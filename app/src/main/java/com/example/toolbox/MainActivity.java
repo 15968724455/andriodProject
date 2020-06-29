163b77d4341e36c65b32336078e7735dfb794eca
imldy <im.ldy@outlook.com>
2020-06-29 13:39:19 +0800

    id前加tbn_

--- app/src/main/java/com/example/toolbox/MainActivity.java
+++ app/src/main/java/com/example/toolbox/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.toolbox;
 
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
