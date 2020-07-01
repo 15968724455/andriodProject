bae75dc34b112dcfade8d834fecc6be6ebe652af
imldy <im.ldy@outlook.com>
2020-07-01 21:53:13 +0800

    APP主界面右上角三个点拉起菜单，点击关于可显示信息

--- app/src/main/java/com/example/toolbox/MainActivity.java
+++ app/src/main/java/com/example/toolbox/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.toolbox;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -45,5 +49,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_tool_04:
                 break;
         }
+    }
+
+    //在主界面label上右上角添加三个点，点击拉起菜单
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, (Menu) menu);//这里是调用menu文件夹中的main.xml，在登陆界面label右上角的三角里显示其他功能
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                // Toast.makeText(MainActivity.this, "关于", 1000).show();
+                // 声明AlertDialog
+                AlertDialog dialog;
+                AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                        .setTitle("关于")
+                        .setIcon(R.mipmap.ic_launcher)
+                        .setMessage("版　本：1.2\n开发者：李德银 于小雨 张守华")
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss(); //关闭对话框
+                            }
+                        });
+                dialog = builder.create();
+                dialog.show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
